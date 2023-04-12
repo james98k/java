f726a3908a0d3e7b152b2f16f24fd2c619798c0e
james98k <54566288+james98k@users.noreply.github.com>
2023-04-12 14:56:33 +0000

    commit for 10773

--- Main.java
+++ Main.java
@@ -1,6 +1,40 @@
+import java.util.*;
+
 class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
+
+    Scanner sc = new Scanner(System.in);
+    int length = sc.nextInt();
+    
+    Stack<Integer> stack = new Stack<>();
+    int sum = 0;
+    int temp = 0;
+
+    for(int i = 0; i < length; i++){
+      temp = sc.nextInt();
+
+      if(temp == 0){
+        temp = stack.pop();
+        System.out.println(temp + "has been poped");
+      }
+      else{
+        stack.push(temp);
+        System.out.println(temp +"has been pushed");
+      }
+
+      // System.out.println(stack[i]);
+    }
+
+    
+
+    for(int i = 0; i < stack.size(); i++){
+      temp = stack.pop();
+      System.out.println(temp + "is added");
+      sum += temp;      
+
+    }
+    System.out.println(sum);
+
   }
 }
